155259e69340510bd67d21fe975b059eccbf5928
Imri Paran <iparan@synamedia.com>
2020-07-25 17:47:15 +0300

    dont use instagram scraper

--- celery-queue/src/scraper.py
+++ celery-queue/src/scraper.py
@@ -23,22 +23,22 @@ def dump_json(obj, dest):
 def scrape(user: str, login_user: str, login_password: str):
     outdir = "output/{}".format(user)
     os.makedirs(outdir, exist_ok=True)
-    profile_path = os.path.join(outdir, "profile.json")
+    # profile_path = os.path.join(outdir, "profile.json")
     followers_path = os.path.join(outdir, "followers.json")
-    if not os.path.exists(profile_path):
-        log.info("running instagram scraper")
-        scraper = InstagramScraper(
-            usernames=[user],
-            destination="temp",
-            media_metadata=True,
-            profile_metadata=True,
-            media_types=[],
-            maximum=10,
-        )
-        scraper.scrape()
-    with open("temp/{u}.json".format(u=user), encoding="utf8") as scraped:
-        profile = json.load(scraped)
-        dump_json(profile, profile_path)
+    # if not os.path.exists(profile_path):
+    #     log.info("running instagram scraper")
+    #     scraper = InstagramScraper(
+    #         usernames=[user],
+    #         destination="temp",
+    #         media_metadata=True,
+    #         profile_metadata=True,
+    #         media_types=[],
+    #         maximum=10,
+    #     )
+    #     scraper.scrape()
+    # with open("temp/{u}.json".format(u=user), encoding="utf8") as scraped:
+    #     profile = json.load(scraped)
+    #     dump_json(profile, profile_path)
     if not os.path.exists(followers_path):
         log.info("running instapy")
         session = InstaPy(
@@ -51,7 +51,7 @@ def scrape(user: str, login_user: str, login_password: str):
         dump_json(followers, followers_path)
     with open(followers_path) as fp:
         followers = json.load(fp)
-    return {"followers": followers, "profile": profile}
+    return {"followers": followers}
 
 
 if __name__ == "__main__":
