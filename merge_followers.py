92e314f8007044bc1a3e84113b12282d3b526191
Imri Paran <iparan@synamedia.com>
2020-09-27 14:16:05 +0300

    fixed script

--- merge_followers.py
+++ merge_followers.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 import json
 import logging
 import os
@@ -6,9 +5,6 @@ import sys
 
 import click
 from dotenv import load_dotenv
-from instapy import smart_run, InstaPy
-
-from utils import upload_to_azure_storage
 
 load_dotenv()
 
@@ -16,19 +12,19 @@ logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 
-def main():
-    path = "C:/tmp/newstory/profiles"
-    users = os.listdir(path)
+@click.command()
+@click.argument("input-dir", type=click.Path(dir_okay=True, file_okay=False))
+@click.argument("out", type=click.File("w"))
+def main(input_dir, out):
+    users = os.listdir(input_dir)
     all = []
     for u in users:
-        relations_path = os.path.join(path, u, "relations.json")
+        relations_path = os.path.join(input_dir, u, "relations.json")
         if os.path.exists(relations_path):
             with open(relations_path) as fp:
                 all.extend(json.load(fp).get("data", {}).get("followers", []))
-    out = "C:/tmp/newstory/youth.txt"
-    os.makedirs(os.path.dirname(out), exist_ok=True)
-    with open(out, "w") as fp:
-        fp.write("\n".join(sorted(all)))
+    os.makedirs(os.path.dirname(out.name), exist_ok=True)
+    out.write("\n".join(sorted(all)))
 
 
 if __name__ == "__main__":
