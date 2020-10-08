d445e6ae4c004ef63a7eebbf6392a50c8a714d76
imri <imri.paran@gmail.com>
2020-10-08 16:56:33 +0300

    fixed web app

--- web/src/routes/auth.ts
+++ web/src/routes/auth.ts
@@ -1,9 +1,8 @@
 import express from "express";
 import { getUser } from "../cotrollers/auth";
-import { keycloak } from "../keycloak";
 
 const router = express.Router();
 
-router.get("/user", keycloak.protect("user"), getUser);
+router.get("/user", getUser);
 
 export default router;
