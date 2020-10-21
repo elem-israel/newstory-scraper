3411ee52f8b918006f6a59c4d671331966a11b6c
imri <imri.paran@gmail.com>
2020-10-21 06:36:53 +0300

    fixed profile scraper

--- followers.py
+++ followers.py
@@ -8,13 +8,13 @@ import click
 from dotenv import load_dotenv
 from instapy import smart_run, InstaPy
 
+from scraper import get_relations
 from utils import upload_to_azure_storage
 
 load_dotenv()
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 logger = logging.getLogger(__name__)
-
 
 
 @click.command()
@@ -34,24 +34,32 @@ def main(users, input_file, out_dir, max_followers, cloud):
         username=os.environ["INSTAPY_USER"],
         password=os.environ["INSTAPY_PASSWORD"],
         headless_browser=True,
+        browser_executable_path="C:/Program Files/Mozilla Firefox/firefox.exe",
     )
     with smart_run(session):
         for u in users_list:
-            print(f"processing {u}")
+            if u.startswith("#"):
+                continue
+            logger.info(f"processing {u}")
             user_dir = os.path.join(out_dir, u)
             user_path = os.path.join(user_dir, "relations.json")
-            relations = get_relations(session, u, max_followers=max_followers)
+            relations = get_relations(session, u, max_followers=max_followers or "full")
             os.makedirs(user_dir, exist_ok=True)
+            created_at = datetime.utcnow()
             with open(user_path, "w") as fp:
-                json.dump(
-                    {"created_at": datetime.utcnow().isoformat(), "data": relations}, fp
-                )
+                json.dump({"created_at": created_at.isoformat(), "data": relations}, fp)
             if cloud:
+                cloud_path = os.path.join(
+                    "profiles",
+                    created_at.strftime("%Y-%m-%d"),
+                    user_dir,
+                    "relations.json",
+                )
                 upload_to_azure_storage(
                     os.environ["AZURE_STORAGE_CONNECTION_STRING"],
                     user_path,
                     os.environ["AZURE_STORAGE_CONTAINER"],
-                    user_path,
+                    cloud_path,
                 )
 
 
