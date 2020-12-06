c52843f77be184c3de19d0c36e7d4c29c8e9f1c5
imri <imri.paran@gmail.com>
2020-12-06 08:28:14 +0200

    cli tool for testing a singular profile loading

--- scripts/jsons_to_sql.py
+++ scripts/jsons_to_sql.py
@@ -10,4 +10,4 @@ from worker.src.tasks import insert_to_db
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
 if __name__ == "__main__":
-    insert_to_db("profiles/2020-11-24/_eden.nnn_/profile.json")
+    insert_to_db(sys.argv[1])
