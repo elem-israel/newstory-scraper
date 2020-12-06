ee6f361998404e805dc646f1fea27ecf30cdb64a
imri <imri.paran@gmail.com>
2020-12-06 08:28:42 +0200

    script for inserting to db

--- scripts/send_to_service.py
+++ scripts/send_to_service.py
@@ -19,26 +19,18 @@ batch = 500
 
 
 def main():
-    with open("D:/tmp/newstory/followers.json", "r") as fp:
-        followers = json.load(fp)
-    profiles = []
-    with open("D:/tmp/newstory/labeled_groups.txt", "r") as fp:
-        for group in fp.readlines():
-            profiles += followers.get(group.strip(), {}).get("data", {}).get("followers", [])
-    profiles = [p.strip() for p in profiles]
-    existing = list(
-        b.name.split("/")[-2]
-        for b in blob_service_client.get_container_client(container_name).list_blobs(
+    blobs = list(
+        blob_service_client.get_container_client(container_name).list_blobs(
             name_starts_with="profiles/2020-11"
         )
     )
-    to_scrape = list(set(profiles) - set(existing))
-    assert len(profiles) > len(to_scrape)
-    shuffle(to_scrape)
-    for user in to_scrape:
-        print(f"sending {user}")
+    # remove duplicates
+    profiles = {b.name.split("/")[-2]: b for b in blobs}
+    for blob in profiles.values():
+        print(f"sending {blob}")
         res = requests.post(
-            f"http://localhost:3030/queue/tasks.profile", json={"args": [user]}
+            f"http://localhost:3030/queue/newstory.tasks.newEntry",
+            json={"message": blob.name},
         )
         res.raise_for_status()
 
