21261d312c28be753cf152dbbe0b2c1af02d652b
imri <imri.paran@gmail.com>
2021-01-13 20:18:46 +0200

    removed api code

--- web/src/cotrollers/auth.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from "express";
-
-export function getUser(req: Request & { kauth: any }, res: Response) {
-  console.log(res.locals);
-  console.log(req);
-  res.send(req.kauth.grant.access_token.content.preferred_username);
-}
--- web/src/routes/queue.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { postQueue, getTaskState } from "../cotrollers/queue";
-
-const router = express.Router();
-
-router.post("/:queue", postQueue);
-router.get("/:queue/:id", getTaskState);
-
-export default router;
