e70b72789d5f8668ecf0ec8b3b3e2fafaada4dc5
imri <imri.paran@gmail.com>
2021-01-13 20:24:32 +0200

    removed api code

--- web/src/redis.ts
+++
@@ -1,9 +0,0 @@
-import redis, { ClientOpts } from "redis";
-
-export const redisConfig: ClientOpts = {
-  host: process.env.REDIS_HOST || "localhost",
-  password: process.env.REDIS_PASSWORD || "",
-  port: Number.parseInt(process.env.REDIS_PORT) || 6379,
-};
-
-export const getRedisClient = () => redis.createClient(redisConfig);
