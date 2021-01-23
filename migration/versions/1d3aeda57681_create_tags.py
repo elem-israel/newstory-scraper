7ba4064aff0d105cad8ee9c20c3f297f49653d55
Imri Paran <iparan@synamedia.com>
2021-01-23 13:46:19 +0200

    added tags to migration

---
+++ migration/versions/1d3aeda57681_create_tags.py
@@ -0,0 +1,24 @@
+"""create tags
+
+Revision ID: 1d3aeda57681
+Revises: 9c37a8bfa028
+Create Date: 2021-01-23 13:40:11.654342
+
+"""
+from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision = '1d3aeda57681'
+down_revision = '9c37a8bfa028'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    with open("sql/2_create_tags.sql") as fp:
+        op.execute(fp.read())
+
+
+def downgrade():
+    pass
