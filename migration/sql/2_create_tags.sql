de22d20c46668b5da5fb19c40a6d78560d125d71
imri <imri.paran@gmail.com>
2021-01-26 19:29:21 +0200

    smaller tag column

--- migration/sql/2_create_tags.sql
+++ migration/sql/2_create_tags.sql
@@ -3,7 +3,7 @@ CREATE TABLE fact_tags
 (
   id                INT IDENTITY (1,1) UNIQUE,
   instagram_post_id VARCHAR(256),
-  tag               NVARCHAR(1024),
+  tag               NVARCHAR(256),
   created_date      DATETIME DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (id)
 );
