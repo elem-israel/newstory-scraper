c940a3e5ec14d24887a618f19503b725ea6abdf8
imri <imri.paran@gmail.com>
2021-02-19 16:53:00 +0200

    encode user before send

--- src/tasks/scrape_profile.py
+++ src/tasks/scrape_profile.py
@@ -10,13 +10,13 @@ from . import bootstrap_servers, profile_storage_path
 logger = logging.getLogger(__name__)
 
 
-def scrape_profile(user) -> dict:
+def scrape_profile(user: str) -> dict:
     profile = get_profile(user, os.path.join(profile_storage_path, "scraper"))
     path = os.path.join(profile_storage_path, "profile", user, "profile.json")
     os.makedirs(os.path.dirname(path), exist_ok=True)
     with open(path, "w") as fp:
         json.dump({"created_at": datetime.utcnow().isoformat(), "data": profile}, fp)
-    get_producer(bootstrap_servers).send("newstory.tasks.upload", user, path).get(
-        timeout=60
-    )
+    get_producer(bootstrap_servers).send(
+        "newstory.tasks.upload", user.encode(), path
+    ).get(timeout=60)
     return profile
