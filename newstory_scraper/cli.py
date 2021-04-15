dcc71bd7380da6f508e82f0fbe52210769eadaee
imri <imri.paran@gmail.com>
2021-04-15 11:55:33 +0300

    cli

--- newstory_scraper/cli.py
+++ newstory_scraper/cli.py
@@ -1,5 +1,32 @@
+import logging
+import sys
+
+import click
+from dotenv import load_dotenv
+
+from newstory_scraper.tasks.scrape_tag import scrape_tag
+
+load_dotenv()
+
+
+@click.group()
 def cli():
-    print("cli")
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+    pass
+
+
+@cli.group()
+def scrape():
+    pass
+
+
+@scrape.command("tag")
+@click.argument("tag")
+def cli_scrape_tag(tag):
+    """
+    Scrape posts by tag from instagram
+    """
+    click.echo(scrape_tag(tag, maximum=1))
 
 
 if __name__ == "__main__":
