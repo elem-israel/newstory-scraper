9e0932e0ae2cd5f8d4fc61e25492be6363757348
imri <imri.paran@gmail.com>
2021-04-16 11:43:27 +0300

    test suite

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,3 +7,8 @@ import pytest
 def profile():
     with open("tests/data/profile1.json") as fp:
         return json.load(fp)
+
+
+@pytest.fixture
+def tags():
+    return json.load(open("tests/data/אנורקסיה.json", encoding="utf8"))
--- tests/test_util.py
+++ tests/test_util.py
@@ -1,6 +1,54 @@
-from util import get_username_by_id
+from datetime import datetime
+
+from newstory_scraper.util import extract_posts
+from newstory_scraper.util import extract_profile
+from newstory_scraper.util import extract_tags
+from newstory_scraper.util import get_username_by_id
 
 
 def test_get_username_by_id():
     res = get_username_by_id(33070920)
     assert res == "pazarbel"
+
+
+def test_extract_profile(profile):
+    extracted = extract_profile(profile)
+    assert extracted == {
+        "created_at": "2020-10-07T13:27:44.758211",
+        "profile_created_at": datetime(2010, 10, 6, 0, 0),
+        "instagram_profile_id": "11472474315",
+        "biography": "He/him\nSelf taught \n🇮🇱\nSupport me on ko fi ?:)",
+        "followers_count": 192,
+        "following_count": 313,
+        "full_name": "Xan (Storm Alexander)",
+        "is_business_account": False,
+        "is_private": False,
+        "posts_count": 42,
+        "profile_pic_url": "https://instagram.ftzl1-1.fna.fbcdn.net/v/t51.2885-19/s150x150/106587108_280349786537053_1306958651287526582_n.jpg?_nc_ht=instagram.ftzl1-1.fna.fbcdn.net&_nc_ohc=KGaVDavuR4IAX_Zbeek&oh=2a1cca6654e9909028f284ab39e8062f&oe=5FA5DB25",
+    }
+
+
+def test_extract_posts(profile):
+    extracted = extract_posts(profile)
+    assert isinstance(extracted[0]["instagram_post_id"], str)
+    assert isinstance(extracted[0]["caption"], str)
+    assert isinstance(extracted[0]["taken_at"], datetime)
+    assert extracted[0]["instagram_author_profile_id"] == "11472474315"
+    assert len(extracted) == 42
+
+
+def test_extract_posts_from_tag_scrape(tags):
+    extracted = extract_posts(tags)
+    assert isinstance(extracted[0]["instagram_post_id"], str)
+    assert isinstance(extracted[0]["caption"], str)
+    assert isinstance(extracted[0]["taken_at"], datetime)
+    assert extracted[0]["instagram_author_profile_id"] == "34392249671"
+    assert len(extracted) == 100
+
+
+def test_extract_hashtags(profile):
+    extracted = extract_tags(profile)
+    for t in extracted:
+        assert isinstance(t["instagram_post_id"], str)
+        assert isinstance(t["tag"], str)
+    assert len(extracted) == 543
