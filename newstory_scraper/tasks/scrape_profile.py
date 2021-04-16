8068d18b21ca659d3da75b80a85f19aec902d798
imri <imri.paran@gmail.com>
2021-04-16 11:52:34 +0300

    library structures

--- newstory_scraper/tasks/scrape_profile.py
+++ newstory_scraper/tasks/scrape_profile.py
@@ -3,20 +3,30 @@ import logging
 import os
 from datetime import datetime
 
-from kafka_config import get_producer
-from scraper import get_profile
-from . import bootstrap_servers, profile_storage_path
+from . import bootstrap_servers
+from ..config import get_config
+from ..kafka_config import get_producer
+from ..scraper import get_profile
 
 logger = logging.getLogger(__name__)
+config = get_config()
+destination = config["scraper"]["destination"]
 
 
-def scrape_profile(user: str) -> dict:
-    profile = get_profile(user, os.path.join(profile_storage_path, "scraper"))
-    path = os.path.join(profile_storage_path, "profile", user, "profile.json")
+def scrape_profile(user) -> dict:
+    """
+    Retrieve a user profile and dump it locally as a json.
+    :param user: The user to scrape
+    :return:
+    """
+    profile = get_profile(user, destination, "scraper")
+    path = os.path.join(destination, "profile", user, "profile.json")
     os.makedirs(os.path.dirname(path), exist_ok=True)
-    with open(path, "w") as fp:
-        json.dump({"created_at": datetime.utcnow().isoformat(), "data": profile}, fp)
-    get_producer(bootstrap_servers).send(
-        topic="newstory.tasks.upload", key=user, value=path
-    ).get(timeout=60)
+    json.dump(
+        {"created_at": datetime.utcnow().isoformat(), "data": profile}, open(path, "w")
+    )
+    if config.getboolean("kafka", "enabled"):
+        get_producer(bootstrap_servers).send(
+            topic="newstory.tasks.upload", key=user, value=path
+        ).get(timeout=60)
     return profile
